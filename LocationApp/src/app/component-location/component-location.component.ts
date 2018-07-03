71417ce3e131fd0cf5799d66fa5a17b548a367b8
shay melamud <shayutut2000@gmail.com>
2018-07-03 15:10:25 +0300

    server

--- LocationApp/src/app/component-location/component-location.component.ts
+++ LocationApp/src/app/component-location/component-location.component.ts
@@ -8,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class ComponentLocationComponent implements OnInit {
   lat: number = 51.678418;
   lng: number = 7.809007;
+  locationChosen=false;
+  zoom: number = 8;
   constructor() { }
 
+  onChoseLocation(event) {
+    console.log(event);
+    this.lat= event.coords.lat;
+    this.lng= event.coords.lng;
+    this.locationChosen=true;
+  }
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+  
+  mapClicked($event) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
   ngOnInit() {
   }
-
+  markers: marker[] = [];
 }
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
+
